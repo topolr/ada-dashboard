ca888f93f4c51451bb3e6571c2a31a932fcfbdaf
Jinliang <hou80houzhu@vip.qq.com>
2019-05-23 23:59:21 +0800

    pass

--- src/modules/listable/state.js
+++ src/modules/listable/state.js
@@ -6,25 +6,21 @@ class ListableService extends Service {
 			cols: [],
 			list: [],
 			tools: [],
-			header: [],
-			body: []
+			_header: [],
+			_body: []
 		};
 	}
 
-	primeKeys() {
-		return ['cols', 'list', 'tools'];
-	}
-
 	onupdate(current, data) {
-		this.assignState(current, data);
+		super.onupdate(current, data);
 		this.getTableData(current);
 	}
 
 	getTableData(current) {
-		current.header = current.cols.map(({ width, title }) => {
+		current._header = current.cols.map(({ width, title }) => {
 			return { title, width };
 		});
-		current.body = current.list.map(item => {
+		current._body = current.list.map(item => {
 			return current.cols.map(({ key, width }) => {
 				return { value: item[key], width, item };
 			});
