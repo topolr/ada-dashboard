077340c6557f43a8713c811b2161e19b69eb9cd3
Jinliang <hou80houzhu@vip.qq.com>
2019-06-02 22:34:10 +0800

    pass

--- src/root.js
+++ src/root.js
@@ -2,18 +2,18 @@ import { root, StaticViewGroup } from 'adajs';
 import './style/index.css';
 import './style/style.css';
 import Container from './main/container';
-import Index from "./main/index";
-// import Login from './main/login';
+// import Index from "./main/index";
+import Login from './main/login';
 
 @root()
 class Root extends StaticViewGroup {
     onready() {
-        this.addChild(Container, {
-            parameter: {
-                indexPage: 'main/index/index.js'
-            }
-        });
-        // this.addChild(Login);
+        // this.addChild(Container, {
+        //     parameter: {
+        //         indexPage: 'main/index/index.js'
+        //     }
+        // });
+        this.addChild(Login);
     }
 }
 
